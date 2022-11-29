8ac5c00005cc219a220352bec00078891e05e9f2
Bryan Moore <bryanallenmoe@gmail.com>
2022-11-29 09:30:21 -0800

    Completed animations

---
+++ main.js
@@ -0,0 +1,31 @@
+/*
+window.onload = () => {
+  const anchors = document.querySelectorAll("a");
+  const transition_el = document.querySelector(".transition");
+
+  setTimeout(() => {
+    transition_el.classList.remove("is-active");
+  }, 200);
+
+  console.log(transition_el);
+
+  console.log(transition_el);
+
+  for (let i = 0; i < anchors.length; i++) {
+    const anchor = anchors[i];
+
+    anchor.addEventListener("click", (e) => {
+      //Every Time you click on a link
+      e.preventDefault(); //Prevent going to the different page
+      let target = e.currentTarget.href; //Store the link name in target variable
+
+      transition_el.classList.add("is-active"); //Add animation class to the page
+
+      setInterval(() => {
+        window.location.href = target; // Wait 500ms, then load the next page.
+      }, 200);
+
+    });
+  }
+};
+*/
